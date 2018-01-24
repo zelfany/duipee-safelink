6c3d01d003cb045c8da158d828a3277371cc056d
zelfany <zelfany@gmail.com>
2018-01-24 10:53:51 +0700

    Add files via upload

---
+++ safelinkrotateleft.js
@@ -0,0 +1,136 @@
+<script>
+<!-- Begin
+var jumlah_iklan = 15;
+var waktu_sekarang = new Date()
+var detik = waktu_sekarang.getSeconds()
+var pasang_iklan = detik % jumlah_iklan;
+pasang_iklan +=1;
+if (pasang_iklan==1) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="conference calling companies"; title="conference calling companies";
+banner="https://2.bp.blogspot.com/-WoddlnWBQt4/Wl3ODD0vjhI/AAAAAAAAIFE/_bGbiN-5fqsXLFBbaBGNntRIvzHc5ia-QCPcBGAYYCw/s1600/3%2BWays%2Bto%2BGet%2BCar%2BInsurance%2BQuotes%2B1.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==2) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="Donate Car for Tax Credit"; title="Donate Car for Tax Credit";
+banner="https://4.bp.blogspot.com/-C5FNTvhf3r4/Wl3OFhCGdVI/AAAAAAAAIFE/QFzoYsmF-zoQc2UHo8NEomqZzYmiQu3jgCPcBGAYYCw/s1600/3%2BWays%2Bto%2BGet%2BCar%2BInsurance%2BQuotes%2B2.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==3) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="Mesothelioma Law Firm"; title="Mesothelioma Law Firm";
+banner="https://4.bp.blogspot.com/-AUfMN_FF9Kk/Wl222TfXYCI/AAAAAAAAIEM/IgQkou1dLg0nd_GiwaWu5ECysZSgpeJJACPcBGAYYCw/s1600/What%2Bto%2BKnow%2BBefore%2BYou%2BSign%2Ban%2BAuto%2BInsurance%2BPolicy%2B2.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==4) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="google structured data"; title="google structured data";
+banner="https://2.bp.blogspot.com/-2hxgyT4YjZs/Wl25u9-mFlI/AAAAAAAAIEc/Rh50zf6V7KoG_zxo1zv1sKQX3MiQk6SuACPcBGAYYCw/s1600/Car%2BInsurance%2Bfor%2Ba%2BNew%2BVehicle%2B1.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==5) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="Donate Car to Charity California"; title="Donate Car to Charity California";
+banner="https://3.bp.blogspot.com/-2WsqBHlVkAc/Wl25w7m6Z3I/AAAAAAAAIEc/Ya2xsXf8Cf0wINqVwRYxu0hkzXLpArBigCPcBGAYYCw/s1600/Car%2BInsurance%2Bfor%2Ba%2BNew%2BVehicle%2B2.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==6) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="Structures Annuity Settlement"; title="Structures Annuity Settlement";
+banner="https://2.bp.blogspot.com/-Nat6MwL7S5E/Wl2_BSUl9zI/AAAAAAAAIEw/FKrhX3aby8IVk0hiMBBA4TMXqOxhAWQcwCPcBGAYYCw/s1600/Tips%2Bto%2BGet%2Ba%2BMore%2BAccurate%2BInsurance%2BQuote%2B1.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==7) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="structured data analytics"; title="structured data analytics";
+banner="https://1.bp.blogspot.com/-nBMaPdHeeTI/Wl2_Co5K6LI/AAAAAAAAIEw/ayDkvVMu3zMfjD9tFI316riq1aEn1MWPQCPcBGAYYCw/s1600/Tips%2Bto%2BGet%2Ba%2BMore%2BAccurate%2BInsurance%2BQuote%2B2.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==8) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="dedicated server hosting providers"; title="dedicated server hosting providers";
+banner="https://2.bp.blogspot.com/-UPURV8vz4M8/Wl220TVuJRI/AAAAAAAAIEM/kdxMuE4BVY0CDfizx9ZDOfgJVHTggwMkACPcBGAYYCw/s1600/What%2Bto%2BKnow%2BBefore%2BYou%2BSign%2Ban%2BAuto%2BInsurance%2BPolicy%2B1.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==9) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="web hosting with database support"; title="web hosting with database support";
+banner="https://3.bp.blogspot.com/-403XRGOo11U/WlcPAYhRSGI/AAAAAAAAID4/tx2KDqUnwZcOMOtV04b-Xl_FyP7EJ4aRwCPcBGAYYCw/s1600/exoclick%2Bleft.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==10) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="best website hosting solutions"; title="best website hosting solutions";
+banner="https://1.bp.blogspot.com/-H494pdM9BgY/WlcO7c82juI/AAAAAAAAID0/OM15qbFogOwr2TTquD7A8Vs7VlX8vk4mgCPcBGAYYCw/s1600/exoclick%2Bright.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==11) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="data preparation for analytics"; title="data preparation for analytics";
+banner="https://4.bp.blogspot.com/-xL6azRO7Bt8/WmbKH79eUaI/AAAAAAAAIHQ/E1cZgm7HZGESiv5L2DYZllC7GM2tW1kugCLcBGAs/s1600/data%2Bpreparation%2Bfor%2Banalytics.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==12) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="download ssdt 2015"; title="download ssdt 2015";
+banner="https://3.bp.blogspot.com/-rtxtmnkB78Q/Wma9k5DgTnI/AAAAAAAAIHE/Fof6b7L0K10fgTKCJhy6BCW6HhGZyr03QCPcBGAYYCw/s1600/download%2Bssdt%2B2015.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==13) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="sql server data tools download 2014"; title="sql server data tools download 2014";
+banner="https://1.bp.blogspot.com/-NwwJ6aIUWUs/WmbKnxedctI/AAAAAAAAIHY/HkGCeLNuaAY-DXI-zZuG9DwVHTezIMPZgCLcBGAs/s1600/sql%2Bserver%2Bdata%2Btools%2Bdownload%2B2014.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==14) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="Download SQL Server Data Tools"; title="Download SQL Server Data Tools";
+banner="https://4.bp.blogspot.com/-9OQ50Lee2fU/WmbK1YA6DnI/AAAAAAAAIHc/dzGTbWZ9kEY-jFi0BnDijjUi7fi-KPo_ACLcBGAs/s1600/Download%2BSQL%2BServer%2BData%2BTools.jpg";
+width="300";
+height="250";
+}
+if (pasang_iklan==15) {
+txt="";
+url="https://go.oclasrv.com/afu.php?zoneid=1524555";
+alt="online backup services"; title="online backup services";
+banner="https://1.bp.blogspot.com/-zJbFnE9Xf7Y/WmbLCO28YpI/AAAAAAAAIHg/ud1a4t0E05Anp_-1PZZFidd46HNtw4UoACLcBGAs/s1600/online%2Bbackup%2Bservices.jpg";
+width="300";
+height="250";
+}
+document.write('<center>');
+document.write('<a href=\"' + url + '\" target=\"_blank\">');
+document.write('<img src=\"' + banner + '\" width=')
+document.write(width + ' height=' + height + ' ');
+document.write('alt=\"' + alt + '\" border=0><br>');
+document.write('<small>' + txt + '</small></a>');
+document.write('</center>');
+// End -->
+</script>
